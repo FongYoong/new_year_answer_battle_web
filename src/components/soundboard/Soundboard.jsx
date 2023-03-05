b6be8b4d8b00ee5c90e831c21d1c2f2a9c1cbdd4
FongYoong <fongyoong8@gmail.com>
2023-03-05 11:18:10 +0800

    Updated theme/win sound in Soundboard

--- src/components/soundboard/Soundboard.jsx
+++ src/components/soundboard/Soundboard.jsx
@@ -1,4 +1,6 @@
+import { useContext } from "react"
 import { Flex } from "@chakra-ui/react"
+import { ConfigContext } from '../contexts/ConfigContext'
 import SoundButton from './SoundButton'
 
 import buttonWinImage from "../../assets/images/button_win.png"
@@ -9,17 +11,19 @@ import buttonSilenceImage from "../../assets/images/button_silence.png"
 
 function Soundboard({...props}) {
 
-    return (
-        <Flex w='100%' justify='flex-end' p={4} {...props} >
-          <Flex id='soundboard' >
-            <SoundButton label='Theme' icon={buttonWinImage} sound='win' />
-            <SoundButton label='Lose' icon={buttonLoseImage} sound='lose' />
-            <SoundButton label='Cheer' icon={buttonCheerImage} sound='cheer' />
-            <SoundButton label='Boo' icon={buttonBooImage} sound='boo' />
-            <SoundButton label='Silence' icon={buttonSilenceImage} sound='silence' />
-          </Flex>
-        </Flex>
-    )
+  const [config, configFunctions] = useContext(ConfigContext);
+
+  return (
+    <Flex w='100%' justify='flex-end' p={4} {...props} >
+      <Flex id='soundboard' >
+        <SoundButton label={config.currentPage == 'home' ? 'Theme' : 'Win'} icon={buttonWinImage} sound={config.currentPage == 'home' ? 'themeRound' : 'win'} />
+        <SoundButton label='Lose' icon={buttonLoseImage} sound='lose' />
+        <SoundButton label='Cheer' icon={buttonCheerImage} sound='cheer' />
+        <SoundButton label='Boo' icon={buttonBooImage} sound='boo' />
+        <SoundButton label='Silence' icon={buttonSilenceImage} sound='silence' />
+      </Flex>
+    </Flex>
+  )
 }
 
 export default Soundboard;
